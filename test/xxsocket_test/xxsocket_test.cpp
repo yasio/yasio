4a0863e609e22cdca31b510f7c8056eae2dc68a3
xseekerj <xseekerj@live.com>
2016-06-07 14:42:54 +0800

    fix issue:#3

--- test/xxsocket_test/xxsocket_test.cpp
+++ test/xxsocket_test/xxsocket_test.cpp
@@ -7,7 +7,7 @@ int main(int, char**)
 
     if (tcpcli.pconnect_n("www.baidu.com", 443, 3/* connect timeout: 3 seconds*/) == 0)
     {
-        printf("connect https serve success.");
+        printf("connect https server success.");
     }
     else {
         printf("connect failed:%s", xxsocket::get_error_msg(xxsocket::get_last_errno()));

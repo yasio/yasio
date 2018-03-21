a17609b2dd85e63ea4eb1540e0bdce7e4044cd5b
halx99 <halx99@live.com>
2018-03-21 16:43:19 +0800

    Update deadline_timer.h

--- src/deadline_timer.h
+++ src/deadline_timer.h
@@ -1,11 +1,11 @@
 //////////////////////////////////////////////////////////////////////////////////////////
 // A cross platform socket APIs, support ios & android & wp8 & window store universal app
-// version: 2.2
+// version: 3.0
 //////////////////////////////////////////////////////////////////////////////////////////
 /*
 The MIT License (MIT)
 
-Copyright (c) 2012-2017 halx99
+Copyright (c) 2012-2018 halx99
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal

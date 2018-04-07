c14aa29a3636032aba05e22fcf0c85905eea3d87
halx99 <halx99@live.com>
2018-04-07 21:48:03 +0800

    set thread name for debugger

--- src/async_tcp_client.cpp
+++ src/async_tcp_client.cpp
@@ -165,6 +165,36 @@ namespace {
         auto duration = std::chrono::steady_clock::now().time_since_epoch();
         return std::chrono::duration_cast<std::chrono::microseconds>(duration).count();
     }
+    
+#if defined(_WIN32)
+    const DWORD MS_VC_EXCEPTION = 0x406D1388;  
+    #pragma pack(push,8)  
+    typedef struct tagTHREADNAME_INFO  
+    {  
+        DWORD dwType; // Must be 0x1000.  
+        LPCSTR szName; // Pointer to name (in user addr space).  
+        DWORD dwThreadID; // Thread ID (-1=caller thread).  
+        DWORD dwFlags; // Reserved for future use, must be zero.  
+     } THREADNAME_INFO;  
+    #pragma pack(pop)  
+    void _set_thread_name(const char* threadName) {  
+        THREADNAME_INFO info;  
+        info.dwType = 0x1000;  
+        info.szName = threadName;  
+        info.dwThreadID = GetCurrentThreadId(); // dwThreadID;  
+        info.dwFlags = 0;  
+    #pragma warning(push)  
+    #pragma warning(disable: 6320 6322)  
+        __try{  
+            RaiseException(MS_VC_EXCEPTION, 0, sizeof(info) / sizeof(ULONG_PTR), (ULONG_PTR*)&info);  
+        }  
+        __except (EXCEPTION_EXECUTE_HANDLER){  
+        }  
+    #pragma warning(pop)  
+    }  
+#else
+    #define _set_thread_name pthread_setname_np
+#endif
 }
 
 class appl_pdu
@@ -384,6 +414,9 @@ void  async_tcp_client::set_endpoint(size_t channel_index, const char* address, 
 
 void async_tcp_client::service()
 { // The async event-loop
+    // Set Thread Name: mini async socket io
+    _set_thread_name("mini-asio");
+    
 #if defined(_WIN32) && !defined(WINRT)
     timeBeginPeriod(1);
 #endif

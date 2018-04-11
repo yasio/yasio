2bed05fb8e6cc907834394e4f085a455c5ad34a7
halx99 <halx99@live.com>
2018-04-11 12:51:05 +0800

    Update xxsocket.h

--- src/xxsocket.h
+++ src/xxsocket.h
@@ -200,9 +200,6 @@ namespace inet {
 // #define _make_dotted_decimal(b1,b2,b3,b4) ( ( ((uint32_t)(b4) << 24) & 0xff000000 ) | ( ((uint32_t)(b3) << 16) & 0x00ff0000 ) | ( ((uint32_t)(b2) << 8) & 0x0000ff00 ) | ( (uint32_t)(b1) & 0x000000ff ) )
 
 static const socket_native_type invalid_socket = (socket_native_type)-1;
-//static const u_long blocking = 0;
-//static const u_long nonblocking = 1;
-
 
 namespace ip {
 
@@ -1026,7 +1023,7 @@ namespace net = inet;
 
 #endif
 /*
-* Copyright (c) 2012-2016 by X.D. Guo  ALL RIGHTS RESERVED.
+* Copyright (c) 2012-2018, HALX99, ALL RIGHTS RESERVED.
 * Consult your license regarding permissions and restrictions.
 **/
 

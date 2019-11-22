cbd153ca14ac9c54d9a4e0f53d8e01606c0f1797
HALX99 <halx99@live.com>
2019-11-22 17:24:56 +0800

    Sync lua example

--- examples/lua/example.lua
+++ examples/lua/example.lua
@@ -1,4 +1,4 @@
--- yasio 3.23.1 demo
+-- yasio 3.30 demo
 require 'protocol_base'
 require 'protocol_enc'
 require 'protocol_dec'
@@ -55,7 +55,7 @@ server:open(1, yasio.YCM_TCP_SERVER)
 local client = io_service.new()
 hostent.host = "127.0.0.1"
 --tcp unpack params, TCP拆包参数设置接口:
-client:set_option(yasio.YOPT_CHANNEL_LFBFD_PARAMS,
+client:set_option(yasio.YOPT_C_LFBFD_PARAMS,
     0, -- channelIndex, 信道索引    
     65535, -- maxFrameLength, 最大包长度
     0,  -- lenghtFieldOffset, 长度字段偏移，相对于包起始字节

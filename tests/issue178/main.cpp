b62151709b63e1be8cc9e7afb29e86302fd7b206
halx99 <halx99@live.com>
2019-12-11 18:12:54 +0800

    add testcase for issue #178

---
+++ tests/issue178/main.cpp
@@ -0,0 +1,64 @@
+#include "yasio/yasio.hpp"
+#include "yasio/obstream.hpp"
+
+using namespace yasio;
+using namespace yasio::inet;
+
+int main()
+{
+  yasio::inet::io_hostent endpoints[] = {
+      {"0.0.0.0", 8010},  // tcp server
+      {"127.0.0.1", 8010} // tcp client
+  };
+  io_service service;
+  deadline_timer delay_timer(service);
+  service.start_service(endpoints, YASIO_ARRAYSIZE(endpoints), [&](event_ptr event) {
+    switch (event->kind())
+    {
+      case YEK_PACKET:
+        if (event->cindex() == 1)
+          printf("Got a packet from server, size=%d\n", (int)event->packet().size());
+        break;
+      case YEK_CONNECT_RESPONSE:
+      {
+        if (event->cindex() == 0)
+        {
+          printf("A client is income, status=%d, %lld, combine 2 packet and send to client!\n", event->status(),
+                 event->timestamp());
+
+          // send 2 packet
+          obstream obs1;
+          obs1.push32();
+          for (auto i = 0; i < 119 - 4; ++i)
+            obs1.write_byte(i + 1);
+          obs1.pop32(obs1.length());
+
+          obstream obs2;
+          obs2.push32();
+          for (auto i = 0; i < 104 - 4; ++i)
+            obs2.write_byte(i + 1);
+          obs2.pop32(obs2.length());
+
+          // merge 2 packets
+          obs1.write_bytes(obs2.data(), obs2.length());
+          service.write(event->transport(), std::move(obs1.buffer()));
+        }
+
+        break;
+      }
+    }
+  });
+  service.set_option(YOPT_S_TIMEOUTS, 5, 5, -1);
+  service.set_option(YOPT_C_LFBFD_PARAMS, 0, 65535, 0, 4, 0);
+  service.set_option(YOPT_C_LFBFD_PARAMS, 1, 65535, 0, 4, 0);
+  service.set_option(YOPT_S_DEFERRED_EVENT, 0); // disable event queue
+  service.open(0, YCM_TCP_SERVER);              // open server client
+
+  delay_timer.expires_from_now(std::chrono::seconds(1));
+    delay_timer.async_wait([&](bool) { 
+        service.open(1, YCM_TCP_CLIENT);
+  });
+
+  getchar();
+  return 0;
+}

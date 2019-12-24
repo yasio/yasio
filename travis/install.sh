4e9972727f1f16c0e2889df067dbe0acc36625ce
HALX99 <halx99@live.com>
2019-12-24 07:21:36 -0800

    Add build android for travis ci  (#194)
    * build android for travis
    
    * make travis install script executable
    
    * convert travis/install.sh line ending to unix style
    
    * fix ci
    
    * fix ci
    
    * update build.sh for travis ci

---
+++ travis/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cmake --version
+
+if [[ "${TRAVIS_OS_NAME}" == "linux" && "${BUILD_TARGET}" == "android" ]]; then
+    curl -o ~/android-ndk.zip https://dl.google.com/android/repository/android-ndk-$NDK_VER-linux-x86_64.zip
+    unzip -q ~/android-ndk.zip -d ~ \
+    "android-ndk-$NDK_VER/build/cmake/*" \
+    "android-ndk-$NDK_VER/build/core/toolchains/arm-linux-androideabi-*/*" \
+    "android-ndk-$NDK_VER/platforms/android-14/arch-arm/*" \
+    "android-ndk-$NDK_VER/source.properties" \
+    "android-ndk-$NDK_VER/sources/android/support/include/*" \
+    "android-ndk-$NDK_VER/sources/cxx-stl/llvm-libc++/libs/armeabi-v7a/*" \
+    "android-ndk-$NDK_VER/sources/cxx-stl/llvm-libc++/include/*" \
+    "android-ndk-$NDK_VER/sysroot/*" \
+    "android-ndk-$NDK_VER/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/*" \
+    "android-ndk-$NDK_VER/toolchains/llvm/prebuilt/linux-x86_64/*"
+fi

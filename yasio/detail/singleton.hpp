2d431ef77489753e75650d1223f0b62092577eca
halx99 <halx99@live.com>
2020-01-11 19:08:43 +0800

    fix #200, optimize singleton implementation.

--- yasio/detail/singleton.hpp
+++ yasio/detail/singleton.hpp
@@ -1,142 +1,155 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+// A cross platform socket APIs, support ios & android & wp8 & window store universal app
 //
-// Copyright (c) 2014-2019 HALX99 - All Rights Reserved
-//
+//////////////////////////////////////////////////////////////////////////////////////////
+/*
+The MIT License (MIT)
+
+Copyright (c) 2012-2020 HALX99
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 #ifndef YASIO__SINGLETON_HPP
 #define YASIO__SINGLETON_HPP
 #include <new>
 #include <memory>
 #include <functional>
 
-#if defined(_ENABLE_MULTITHREAD)
+#if defined(YASIO_ENABLE_CONCURRENT_SINGLETON)
 #  include <mutex>
+#  include <atomic>
 #endif
 
 namespace yasio
 {
 namespace gc
 {
-
-// CLASS TEMPLATE singleton forward decl
-template <typename _Ty, bool delayed = false> class singleton;
-
-/// CLASS TEMPLATE singleton
-/// the managed singleton object will be destructed after main function.
-template <typename _Ty> class singleton<_Ty, false>
+template <typename _Ty, bool delay = false> class singleton_constructor
 {
-  typedef singleton<_Ty, false> _Myt;
+public:
+  template <typename... _Args> static _Ty* construct(_Args&&... args)
+  {
+    return new (std::nothrow) _Ty(std::forward<_Args>(args)...);
+  }
+};
+
+template <typename _Ty> class singleton_constructor<_Ty, true>
+{
+public:
+  template <typename... _Args> static _Ty* construct(_Args&&... args)
+  {
+    auto inst = new (std::nothrow) _Ty();
+    if (inst)
+      delay_init(inst, std::forward<_Args>(args)...);
+    return inst;
+  }
+
+private:
+  template <typename _Fty, typename... _Args>
+  static void delay_init(_Ty* inst, _Fty&& memf, _Args&&... args)
+  { // init use specific member func with more than 1 args
+    std::mem_fn(memf)(inst, std::forward<_Args>(args)...);
+  }
+
+  template <typename _Fty, typename _Arg> static void delay_init(_Ty* inst, _Fty&& memf, _Arg&& arg)
+  { // init use specific member func with 1 arg
+    std::mem_fn(memf)(inst, std::forward<_Arg>(arg));
+  }
+
+  template <typename _Fty> static void delay_init(_Ty* inst, _Fty&& memf)
+  { // init use specific member func without arg
+    std::mem_fn(memf)(inst);
+  }
+
+  static void delay_init(_Ty* inst)
+  { // dummy init without delay init member func, same as no delay constructor
+  }
+};
+
+/// CLASS TEMPLATE singleton, support non-delayed or delayed init with variadic args
+/// the managed singleton object will be destructed after main function.
+template <typename _Ty> class singleton
+{
+  typedef singleton<_Ty> _Myt;
   typedef _Ty* pointer;
 
 public:
+  // Return a singleton instance
   template <typename... _Args> static pointer instance(_Args&&... args)
   {
     if (_Myt::__single__)
-      return _Myt::__single__.get();
+      return _Myt::__single__;
 
-#if defined(_ENABLE_MULTITHREAD)
-    _Myt::__mutex__.lock();
+#if defined(YASIO_ENABLE_CONCURRENT_SINGLETON)
+    std::lock_guard<std::mutex> lck(__mutex__);
+    if (_Myt::__single__)
+      return _Myt::__single__;
 #endif
-    _Myt::__single__.reset(new (std::nothrow) _Ty(args...));
-#if defined(_ENABLE_MULTITHREAD)
-    _Myt::__mutex__.unlock();
-#endif
+    return (_Myt::__single__ = singleton_constructor<_Ty>::construct(std::forward<_Args>(args)...));
+  }
 
-    return _Myt::__single__.get();
+  // Return a singleton instance with delayed init func
+  template <typename... _Args> static pointer delayed(_Args&&... args)
+  {
+    if (_Myt::__single__)
+      return _Myt::__single__;
+
+#if defined(YASIO_ENABLE_CONCURRENT_SINGLETON)
+    std::lock_guard<std::mutex> lck(__mutex__);
+    if (_Myt::__single__)
+      return _Myt::__single__;
+#endif
+    return (_Myt::__single__ =
+                singleton_constructor<_Ty, true>::construct(std::forward<_Args>(args)...));
   }
 
   static void destroy(void)
   {
+#if defined(YASIO_ENABLE_CONCURRENT_SINGLETON)
+    std::lock_guard<std::mutex> lck(__mutex__);
+#endif
     if (_Myt::__single__)
     {
-      _Myt::__single__.reset();
+      delete _Myt::__single__;
+      _Myt::__single__ = nullptr;
     }
   }
 
 private:
-  static std::unique_ptr<_Ty> __single__;
-#if defined(_ENABLE_MULTITHREAD)
+#if !defined(YASIO_ENABLE_CONCURRENT_SINGLETON)
+  static _Ty* __single__;
+#else
+  static std::atomic<_Ty*> __single__;
   static std::mutex __mutex__;
 #endif
 private:
-  // static class: disable construct, assign operation, copy construct also not allowed.
+  // disable construct, assign operation, copy construct also not allowed.
   singleton(void) = delete;
 };
 
-/// CLASS TEMPLATE singleton, support delay init with variadic args
-/// the managed singleton object will be destructed after main function.
-template <typename _Ty> class singleton<_Ty, true>
-{
-  typedef singleton<_Ty, true> _Myt;
-  typedef _Ty* pointer;
-
-public:
-  template <typename... _Args> static pointer instance(_Args&&... args)
-  {
-    if (_Myt::__single__)
-      return _Myt::__single__.get();
-
-#if defined(_ENABLE_MULTITHREAD)
-    _Myt::__mutex__.lock();
+#if !defined(YASIO_ENABLE_CONCURRENT_SINGLETON)
+template <typename _Ty> _Ty* singleton<_Ty>::__single__;
+#else
+template <typename _Ty> std::atomic<_Ty*> singleton<_Ty>::__single__;
+template <typename _Ty> std::mutex singleton<_Ty>::__mutex__;
 #endif
 
-    _Myt::__single__.reset(new (std::nothrow) _Ty());
-    if (_Myt::__single__)
-      _Myt::delay_init(args...);
-#if defined(_ENABLE_MULTITHREAD)
-    _Myt::__mutex__.unlock();
-#endif
-    return _Myt::__single__.get();
-  }
-
-  static void destroy(void)
-  {
-    if (_Myt::__single__)
-    {
-      _Myt::__single__.reset();
-    }
-  }
-
-private:
-  template <typename _Fty, typename... _Args> static void delay_init(_Fty&& memf, _Args&&... args)
-  { // init use specific member func with more than 1 args
-    std::mem_fn(memf)(_Myt::__single__.get(), args...);
-  }
-
-  template <typename _Fty, typename _Arg> static void delay_init(_Fty&& memf, _Arg&& arg)
-  { // init use specific member func with 1 arg
-    std::mem_fn(memf)(_Myt::__single__.get(), arg);
-  }
-
-  template <typename _Fty> static void delay_init(_Fty&& memf)
-  { // init use specific member func without arg
-    std::mem_fn(memf)(_Myt::__single__.get());
-  }
-
-  static void delay_init(void)
-  { // dummy init
-  }
-
-private:
-  static std::unique_ptr<_Ty> __single__;
-#if defined(_ENABLE_MULTITHREAD)
-  static std::mutex __mutex__;
-#endif
-private:
-  singleton(void) =
-      delete; // just disable construct, assign operation, copy construct also not allowed.
-};
-
-template <typename _Ty> std::unique_ptr<_Ty> singleton<_Ty, false>::__single__;
-#if defined(_ENABLE_MULTITHREAD)
-template <typename _Ty> std::mutex singleton<_Ty, false>::__mutex__;
-#endif
-
-template <typename _Ty> std::unique_ptr<_Ty> singleton<_Ty, true>::__single__;
-#if defined(_ENABLE_MULTITHREAD)
-template <typename _Ty> std::mutex singleton<_Ty, true>::__mutex__;
-#endif
-
-// TEMPLATE alias
-template <typename _Ty> using delayed = singleton<_Ty, true>;
 } // namespace gc
 } // namespace yasio
 

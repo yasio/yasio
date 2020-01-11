dd2cdff2bd436aab66869dccf65088bcff459853
halx99 <halx99@live.com>
2020-01-12 01:11:54 +0800

    fix issue201 print message.

--- tests/issue201/main.cpp
+++ tests/issue201/main.cpp
@@ -9,15 +9,15 @@ int main()
   xxsocket sock;
   const char* addrip = "114.114.114.114";
   u_short port       = 9099;
-  YASIO_LOG("connecting %s:u...", addrip, port);
+  YASIO_LOG("connecting %s:%u...", addrip, port);
   auto start = yasio::highp_clock();
   if (sock.xpconnect_n(addrip, port, std::chrono::seconds(3)) != 0)
-    printf("connect %s succeed failed, the timeout should be approximately equal to 3(s), real: "
+    YASIO_LOG("connect %s failed, the timeout should be approximately equal to 3(s), real: "
            "%lf(s); but when the host network adapter isn't connected, it may be approximately "
            "equal to 0(s)\n",
            addrip, (yasio::highp_clock() - start) / (double)MICROSECONDS_PER_SECOND);
   else
-    printf("Aha connect %s succeed, cost: %lf(s)\n", addrip,
+    YASIO_LOG("Aha connect %s succeed, cost: %lf(s)\n", addrip,
            (yasio::highp_clock() - start) / (double)MICROSECONDS_PER_SECOND);
   return 0;
 }

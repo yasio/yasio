d50170ed34b7092b3ad4f77fd3627be3337bf9a3
HALX99 <halx99@live.com>
2020-01-18 01:41:55 +0800

    Use default value instead.

--- yasio/yasio.hpp
+++ yasio/yasio.hpp
@@ -644,16 +644,13 @@ public:
 
   YASIO__DECL io_channel* cindex_to_handle(size_t cindex) const;
 
-  int write(transport_handle_t thandle, const void* buf, size_t len)
+  int write(transport_handle_t thandle, const void* buf, size_t len,
+                        std::function<void()> handler = nullptr)
   {
-    return write(thandle, std::vector<char>((char*)buf, (char*)buf + len));
-  }
-  int write(transport_handle_t thandle, std::vector<char> buffer)
-  {
-    return write(thandle, std::move(buffer), nullptr);
+    return write(thandle, std::vector<char>((char*)buf, (char*)buf + len), std::move(handler));
   }
   YASIO__DECL int write(transport_handle_t thandle, std::vector<char> buffer,
-                        std::function<void()>);
+                        std::function<void()> = nullptr);
 
   // The deadlien_timer support, !important, the callback is called on the thread of io_service
   deadline_timer_ptr schedule(highp_time_t duration, timer_cb_t cb)

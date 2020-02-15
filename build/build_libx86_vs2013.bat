bf0ea5a782a10a1d3acfd74ac4d789e7bdbeef69
halx99 <halx99@live.com>
2020-02-15 13:35:30 +0800

    Update build libx86 scripts.

--- build/build_libx86_vs2013.bat
+++ build/build_libx86_vs2013.bat
@@ -6,5 +6,5 @@ if not exist build_x86 mkdir build_x86
 cd build_x86
 cmake -G "Visual Studio 12 2013" ../../
 cmake --build . --config Debug --target yasio
-
+cmake --build . --config Release --target yasio
 popd
--- build/build_libx86_vs2019.bat
+++ build/build_libx86_vs2019.bat
@@ -5,6 +5,6 @@ pushd %~dp0
 if not exist build_x86 mkdir build_x86
 cd build_x86
 cmake -A Win32 ../../
-cmake --build . --config Debug --target yasio
-
+cmake --build . --config Debug --target yasio --clean-first
+cmake --build . --config Release --target yasio
 popd

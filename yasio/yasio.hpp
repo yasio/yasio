472e02ef7fac3ba995ef73d998d039dca725efa9
halx99 <halx99@live.com>
2020-05-01 21:01:46 +0800

    fix header only link issue

--- yasio/yasio.hpp
+++ yasio/yasio.hpp
@@ -753,7 +753,7 @@ public:
   **   b.this function only works once
   **   c. you should call once before call any 'io_servic::start'
   */
-  static void init_globals(const yasio::inet::print_fn_t&);
+  YASIO__DECL static void init_globals(const yasio::inet::print_fn_t&);
 
   /*
   ** Summary: cleanup necessary global state data, such custom print function.
@@ -763,7 +763,8 @@ public:
   **      custom print function.
   **   c. you should ensure there no any io_service running whe call this function
   */
-  static void cleanup_globals();
+  YASIO__DECL static void cleanup_globals();
+
 public:
   YASIO__DECL io_service();
   YASIO__DECL io_service(int channel_count);

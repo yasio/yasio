852b52a4e540da0f9e29fc7213dfe5bee0abacce
halx99 <halx99@live.com>
2020-05-20 13:05:14 +0800

    Rename kcptest to speedtest, since it support test both tcp,udp,kcp transfer speed.

--- tests/speed/main.cpp
+++ tests/speed/main.cpp
@@ -13,12 +13,32 @@ using namespace yasio::inet;
 
 // KCP speed: 1.3MB/s(windows), 1.8MB/s(linux)
 // UDP speed: 1.3GB/s(windows), 2.2GB/s(linux)
-#define USE_KCP 1
 
-#if USE_KCP
-#  define TRANSFER_PROTOCOL YCK_KCP_CLIENT
+#define YPROTO_TCP 1
+#define YPROTO_UDP 2
+#define YPROTO_KCP 3
+
+#define TRANSFER_PROTOCOL YPROTO_TCP
+
+#if TRANSFER_PROTOCOL == YPROTO_KCP
+#  define RECEIVER_CHANNEL_KIND YCK_KCP_CLIENT
+#  define SENDER_CHANNEL_KIND YCK_KCP_CLIENT
+#elif TRANSFER_PROTOCOL == YPROTO_UDP
+#  define RECEIVER_CHANNEL_KIND YCK_UDP_CLIENT
+#  define SENDER_CHANNEL_KIND YCK_UDP_CLIENT
+#elif TRANSFER_PROTOCOL == YPROTO_TCP
+#  define RECEIVER_CHANNEL_KIND YCK_TCP_SERVER
+#  define SENDER_CHANNEL_KIND YCK_TCP_CLIENT
 #else
-#  define TRANSFER_PROTOCOL YCK_UDP_CLIENT
+#  error "please define TRANSFER_PROTOCOL to one of YPROTO_TCP, YPROTO_UDP, YPROTO_KCP"
+#endif
+
+#if TRANSFER_PROTOCOL == YPROTO_TCP
+#  define RECEIVER_PORT 3001
+#  define SENDER_PORT 3001
+#else
+#  define RECEIVER_PORT 3001
+#  define SENDER_PORT 3002
 #endif
 
 static const double s_send_limit_time = 10; // max send time in seconds
@@ -28,6 +48,12 @@ static long long s_recv_total_bytes = 0;
 
 static double s_send_speed = 0; // bytes/s
 static double s_recv_speed = 0;
+
+static const char* proto_name(int myproto)
+{
+  static const char* protos[] = {"null", "TCP", "UDP", "KCP"};
+  return protos[myproto];
+}
 
 static void sbtoa(double speedInBytes, char* buf)
 {
@@ -72,14 +98,14 @@ void udp_send_repeat_forever(io_service* service, transport_handle_t thandle, ob
   static double last_print_time = 0;
 
   s_send_total_bytes += obs->length();
-  auto cb                       = [=](int, size_t bytes_transferred) {
+  auto cb = [=](int, size_t bytes_transferred) {
     time_elapsed = (yasio::highp_clock<>() - time_start) / 1000000.0;
     s_send_speed = s_send_total_bytes / time_elapsed;
     udp_send_repeat_forever(service, thandle, obs);
   };
 
   if (time_elapsed < s_send_limit_time)
-     service->write(thandle, obs->buffer(), cb);
+    service->write(thandle, obs->buffer(), cb);
 }
 
 void kcp_send_repeat_forever(io_service* service, transport_handle_t thandle, obstream* obs)
@@ -104,7 +130,7 @@ void kcp_send_repeat_forever(io_service* service, transport_handle_t thandle, ob
 void start_sender(io_service& service)
 {
   static const int PER_PACKET_SIZE =
-      TRANSFER_PROTOCOL == YCK_KCP_CLIENT ? YASIO_SZ(62, k) : YASIO_SZ(63, k);
+      TRANSFER_PROTOCOL == YPROTO_KCP ? YASIO_SZ(62, k) : YASIO_SZ(63, k);
   static char buffer[PER_PACKET_SIZE];
   static obstream obs;
   obs.write_bytes(buffer, PER_PACKET_SIZE);
@@ -129,7 +155,7 @@ void start_sender(io_service& service)
           if (ec != 0)
             YASIO_LOG("set_option failed, ec=%d, detail:%s", ec, xxsocket::strerror(ec));
 
-          if (TRANSFER_PROTOCOL == YCK_KCP_CLIENT)
+          if (TRANSFER_PROTOCOL == YPROTO_KCP)
           {
             setup_kcp_transfer(thandle);
             kcp_send_repeat_forever(&service, thandle, &obs);
@@ -144,10 +170,12 @@ void start_sender(io_service& service)
     }
   });
 
+  printf("start trasnfer test via %s after 170ms...\n", proto_name(TRANSFER_PROTOCOL));
   std::this_thread::sleep_for(std::chrono::milliseconds(170));
-
-  service.set_option(YOPT_C_LOCAL_PORT, 0, 30001);
-  service.open(0, TRANSFER_PROTOCOL);
+#if TRANSFER_PROTOCOL != YPROTO_TCP
+  service.set_option(YOPT_C_LOCAL_PORT, 0, RECEIVER_PORT);
+#endif
+  service.open(0, SENDER_CHANNEL_KIND);
 }
 
 static io_service* s_sender;
@@ -176,7 +204,7 @@ void start_receiver(io_service& service)
           if (ec != 0)
             YASIO_LOG("set_option failed, ec=%d, detail:%s", ec, xxsocket::strerror(ec));
 
-          if (TRANSFER_PROTOCOL == YCK_KCP_CLIENT)
+          if (TRANSFER_PROTOCOL == YPROTO_KCP)
             setup_kcp_transfer(event->transport());
           printf("start recive data...\n");
         }
@@ -187,13 +215,15 @@ void start_receiver(io_service& service)
     }
   });
 
-  service.set_option(YOPT_C_LOCAL_PORT, 0, 30002);
-  service.open(0, TRANSFER_PROTOCOL);
+#if TRANSFER_PROTOCOL != YPROTO_TCP
+  service.set_option(YOPT_C_LOCAL_PORT, 0, SENDER_PORT);
+#endif
+  service.open(0, RECEIVER_CHANNEL_KIND);
 }
 
 int main(int, char**)
 {
-  io_hostent receiver_ep("127.0.0.1", 30001), sender_ep("127.0.0.1", 30002);
+  io_hostent receiver_ep("127.0.0.1", RECEIVER_PORT), sender_ep("127.0.0.1", SENDER_PORT);
   io_service receiver(&receiver_ep, 1), sender(&sender_ep, 1);
 
   s_sender = &sender;

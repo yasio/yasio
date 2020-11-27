a58cf918850efdff23846e57b389fd1c182573cd
HALX99 <halx99@live.com>
2020-11-27 09:37:54 +0800

    Update ibstream.cpp

--- yasio/ibstream.cpp
+++ yasio/ibstream.cpp
@@ -152,7 +152,7 @@ uint32_t ibstream_view::read_u24()
 
 cxx17::string_view ibstream_view::read_v()
 {
-  int count = read_ix<>();
+  int count = read_ix<int>();
   return read_bytes(count);
 }
 
@@ -179,10 +179,9 @@ void ibstream_view::read_bytes(void* oav, int len)
 
 cxx17::string_view ibstream_view::read_bytes(int len)
 {
-  cxx17::string_view sv;
   if (len > 0)
-    sv = cxx17::string_view(consume(len), len);
-  return sv;
+    return cxx17::string_view(consume(len), len);
+  return cxx17::string_view{};
 }
 
 const char* ibstream_view::consume(size_t size)

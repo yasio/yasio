e0a623b00f9ee9354c31bb38819f29b0080e2db9
halx99 <halx99@live.com>
2020-12-07 19:38:57 +0800

    Add yasio_uelua_init for more easy to integrate to unreal engine

---
+++ yasio/bindings/yasio_uelua.cpp
@@ -0,0 +1,48 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+// A cross platform socket APIs, support ios & android & wp8 & window store
+// universal app
+//////////////////////////////////////////////////////////////////////////////////////////
+/*
+The MIT License (MIT)
+
+Copyright (c) 2012-2020 HALX99
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#include "yasio/platform/yasio_ue4.hpp"
+#include "lua.hpp"
+#if defined(NS_SLUA)
+using namespace NS_SLUA;
+#endif
+#include "yasio/bindings/lyasio.cpp"
+
+DECLARE_LOG_CATEGORY_EXTERN(yasio_ue4, Log, All);
+DEFINE_LOG_CATEGORY(yasio_ue4);
+
+void yasio_uelua_init(void* L) {
+    auto Ls = (lua_State*)L;
+	print_fn2_t log_cb = [](int level, const char* msg) {
+		FString text(msg);
+		const TCHAR* tstr = *text;
+		UE_LOG(yasio_ue4, Log, L"%s", tstr);
+	};
+	io_service::init_globals(log_cb);
+
+	luaregister_yasio(Ls);
+}

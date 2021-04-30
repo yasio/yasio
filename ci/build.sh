b5216e9417d6523a531bc5dece02c57dd92b8cb9
halx99 <halx99@live.com>
2021-04-30 16:04:15 +0800

    Optimize cmake script [ci build]

--- ci/build.sh
+++ ci/build.sh
@@ -12,7 +12,7 @@ function build_osx()
     
     mkdir -p build_osx
     
-    cmake .. -GXcode -Bbuild_osx -DYASIO_SSL_BACKEND=1 -DYASIO_BUILD_WITH_CARES=ON
+    cmake .. -GXcode -Bbuild_osx -DYASIO_SSL_BACKEND=1 -DYASIO_HAVE_CARES=ON
     cmake --build build_osx --config Release
     
     echo "run test tcptest on osx..."
@@ -28,10 +28,10 @@ function build_ios()
     
     mkdir -p build_ios
     
-    # cmake .. -GXcode -Bbuild_ios -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_SYSROOT=iphonesimulator -DYASIO_SSL_BACKEND=1 -DYASIO_BUILD_WITH_CARES=ON
+    # cmake .. -GXcode -Bbuild_ios -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_SYSROOT=iphonesimulator -DYASIO_SSL_BACKEND=1 -DYASIO_HAVE_CARES=ON
     # cmake official cross-compiling doesn't works on latest macOS 10.15 + xcode11
     # https://cmake.org/cmake/help/v3.17/manual/cmake-toolchains.7.html?highlight=iphoneos#cross-compiling-for-ios-tvos-or-watchos
-    cmake .. -GXcode -Bbuild_ios -DCMAKE_SYSTEM_NAME=iOS "-DCMAKE_OSX_ARCHITECTURES=armv7;arm64" -DYASIO_SSL_BACKEND=1 -DYASIO_BUILD_WITH_CARES=ON
+    cmake .. -GXcode -Bbuild_ios -DCMAKE_SYSTEM_NAME=iOS "-DCMAKE_OSX_ARCHITECTURES=armv7;arm64" -DYASIO_SSL_BACKEND=1 -DYASIO_HAVE_CARES=ON
     cmake --build build_ios --config Release
     
     exit 0
@@ -42,7 +42,7 @@ function build_linux()
     echo "Building linux..."
     cd $YASIO_ROOT/build
     mkdir -p build_linux
-    cmake .. -G "Unix Makefiles" -Bbuild_linux -DCMAKE_BUILD_TYPE=Release -DYASIO_SSL_BACKEND=2 -DYASIO_BUILD_WITH_KCP=ON -DYASIO_BUILD_WITH_CARES=ON -DYAISO_BUILD_NI=YES -DCXX_STD=17 -DYASIO_VERBOSE_LOG=ON
+    cmake .. -G "Unix Makefiles" -Bbuild_linux -DCMAKE_BUILD_TYPE=Release -DYASIO_SSL_BACKEND=2 -DYASIO_HAVE_KCP=ON -DYASIO_HAVE_CARES=ON -DYAISO_BUILD_NI=YES -DCXX_STD=17 -DYASIO_VERBOSE_LOG=ON
     cmake --build build_linux -- -j `nproc`
     
     echo "run test issue201 on linux..."
@@ -56,7 +56,7 @@ function build_freebsd()
     echo "Building freebsd..."
     cd $YASIO_ROOT/build
     mkdir -p build_freebsd
-    cmake .. -G "Unix Makefiles" -Bbuild_freebsd -DCMAKE_BUILD_TYPE=Release -DYASIO_SSL_BACKEND=0 -DYASIO_BUILD_WITH_CARES=ON
+    cmake .. -G "Unix Makefiles" -Bbuild_freebsd -DCMAKE_BUILD_TYPE=Release -DYASIO_SSL_BACKEND=0 -DYASIO_HAVE_CARES=ON
     cmake --build build_freebsd -- -j `nproc`
     
     echo "run test issue201 on freebsd..."
@@ -70,7 +70,7 @@ function build_android()
     echo "Building android..."
     cd $YASIO_ROOT/build
     mkdir -p build_armv7
-    cmake .. -G "Unix Makefiles" -Bbuild_armv7 -DANDROID_STL=c++_shared -DCMAKE_TOOLCHAIN_FILE=~/android-ndk-$NDK_VER/build/cmake/android.toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DYASIO_SSL_BACKEND=1 -DYASIO_BUILD_WITH_CARES=ON
+    cmake .. -G "Unix Makefiles" -Bbuild_armv7 -DANDROID_STL=c++_shared -DCMAKE_TOOLCHAIN_FILE=~/android-ndk-$NDK_VER/build/cmake/android.toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DYASIO_SSL_BACKEND=1 -DYASIO_HAVE_CARES=ON
     cmake --build build_armv7 --target yasio
     
     exit 0

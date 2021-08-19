0477971a474332eec69b7e692f727f8925afa13a
halx99 <halx99@live.com>
2021-08-19 12:08:32 +0800

    Update install.sh

--- ci/install.sh
+++ ci/install.sh
@@ -3,7 +3,7 @@ cmake --version
 
 if [[ "${TRAVIS_OS_NAME}" == "linux" && "${BUILD_TARGET}" == "android" ]]; then
     curl -o ~/android-ndk-$NDK_VER-linux-x86_64.zip https://dl.google.com/android/repository/android-ndk-$NDK_VER-linux-x86_64.zip
-    unzip ~/android-ndk-$NDK_VER-linux-x86_64.zip -d ~
+    unzip -q ~/android-ndk-$NDK_VER-linux-x86_64.zip -d ~
     wget -O ~/ninja-linux.zip https://github.com/ninja-build/ninja/releases/download/v1.10.2/ninja-linux.zip
     unzip ~/ninja-linux.zip -d ~
     ~/ninja --version

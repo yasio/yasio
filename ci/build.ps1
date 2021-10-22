5d87f922ddb42d8e19bd7c29473bae8046a45ca8
halx99 <halx99@live.com>
2021-10-22 12:40:37 +0800

    Add mingw-ci

--- ci/build.ps1
+++ ci/build.ps1
@@ -1,17 +1,38 @@
-# install latest cmake version
-curl https://github.com/Kitware/CMake/releases/download/v3.20.2/cmake-3.20.2-windows-x86_64.zip -o cmake-3.20.2-windows-x86_64.zip
-Expand-Archive -Path cmake-3.20.2-windows-x86_64.zip -DestinationPath .\
-$cmake_bin = (Resolve-Path .\cmake-3.20.2-windows-x86_64\bin).Path
-$env:Path = "$cmake_bin;$env:Path"
+# Install latest cmake version for appveyor ci
+if (!($env:GITHUB_CI -eq "true")) {
+    $cmake_ver=3.21.3
+    curl https://github.com/Kitware/CMake/releases/download/v$cmake_ver/cmake-$cmake_ver-windows-x86_64.zip -o cmake-$cmake_ver-windows-x86_64.zip
+    Expand-Archive -Path cmake-$cmake_ver-windows-x86_64.zip -DestinationPath .\
+    $cmake_bin = (Resolve-Path .\cmake-$cmake_ver-windows-x86_64\bin).Path
+    $env:Path = "$cmake_bin;$env:Path"
+}
+
 cmake --version
 
-# run build
-echo "env:ARCH=$env:ARCH"
-if($env:ARCH -eq "x64") {
-    cmake -G "Visual Studio 12 2013 Win64" -B build -DYASIO_BUILD_WITH_LUA=ON
+# Generate
+echo "env:BUILD_ARCH=$env:BUILD_ARCH"
+
+if ($env:GITHUB_CI -eq "true") {
+    if ($env:TOOLCHAIN -eq "msvc") { # Generate vs2019 on github ci
+       if ($env:BUILD_ARCH -eq "x64") {
+           cmake -B build -DYASIO_SSL_BACKEND=1
+       }
+       else {
+           cmake -A Win32 -B build -DYASIO_SSL_BACKEND=1
+       }
+    }
+    else { # Generate mingw
+        cmake -G "MinGW Makefiles" -B build
+    }
 }
-else {
-    cmake -G "Visual Studio 12 2013" -B build -DYASIO_BUILD_WITH_LUA=ON
+else { # Generate vs2013 on appveyor ci
+    if ($env:BUILD_ARCH -eq "x64") {
+        cmake -G "Visual Studio 12 2013 Win64" -B build -DYASIO_BUILD_WITH_LUA=ON
+    }
+    else {
+        cmake -G "Visual Studio 12 2013" -B build -DYASIO_BUILD_WITH_LUA=ON
+    }
 }
 
-cmake --build build --config Release --clean-first
+# Build
+cmake --build build --config $env:BUILD_TYPE

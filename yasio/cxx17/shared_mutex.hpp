88a32649028c5e9f5e1065d4fba1c3c9c3fdfc03
halx99 <halx99@live.com>
2021-10-25 15:33:45 +0800

    Update source copyright notice

--- yasio/cxx17/shared_mutex.hpp
+++ yasio/cxx17/shared_mutex.hpp
@@ -2,28 +2,16 @@
 // A multi-platform support c++11 library with focus on asynchronous socket I/O for any
 // client application.
 //////////////////////////////////////////////////////////////////////////////////////////
-/*
-The MIT License (MIT)
-
-Copyright (c) 2012-2021 HALX99
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+//
+// Copyright (c) 2021 halx99 (halx99 at live dot com)
+// Copyright Vicente J. Botet Escriba 2012.
+// Copyright Howard Hinnant 2007-2010.
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// see also: https://github.com/boostorg/thread/blob/develop/include/boost/thread/v2/shared_mutex.hpp
+// 
 
 #ifndef YASIO__SHARED_MUTEX_HPP
 #define YASIO__SHARED_MUTEX_HPP
@@ -40,7 +28,7 @@ SOFTWARE.
 #  define yaso__throw_error(e) YASIO__THROW0(std::system_error(std::make_error_code(e), ""))
 namespace cxx17
 {
-// CLASS TEMPLATE shared_mutex, copy from: https://github.com/boostorg/thread/blob/develop/include/boost/thread/v2/shared_mutex.hpp
+// CLASS TEMPLATE shared_mutex
 class shared_mutex {
   typedef std::mutex mutex_t;
   typedef std::condition_variable cond_t;

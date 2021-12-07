644bf54dee6d89f08deda1853ae4c4f5461940e7
halx99 <halx99@live.com>
2021-12-07 15:21:21 +0800

    Update yasio_ni.cpp

--- yasio/bindings/yasio_ni.cpp
+++ yasio/bindings/yasio_ni.cpp
@@ -27,7 +27,7 @@ SOFTWARE.
 */
 
 /*
-** yasio_ni v2: The yasio native interface for interop, match OpenNSM2(The Unity C# wrapper of yasio)
+** yasio_ni v2: The yasio native interface for interop, match with `yasio/yasio_unity`
 */
 #include <array>
 #include <string.h>

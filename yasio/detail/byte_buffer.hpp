1aa0064ac6d7faf624c594140dd33b2a1d223d1c
halx99 <halx99@live.com>
2021-12-11 12:08:58 +0800

    Improve code style

--- yasio/detail/byte_buffer.hpp
+++ yasio/detail/byte_buffer.hpp
@@ -28,8 +28,8 @@ SOFTWARE.
 
 /* The byte_buffer concepts
    a. The memory model is similar to to std::vector<char>, std::string
-   b. use c realloc/free to manage memory
-   c. implemented operations:
+   b. Use c realloc to manage memory
+   c. Implemented operations:
       - resize(without fill)
       - resize_fit
       - attach/detach(stl not support)
@@ -77,9 +77,10 @@ public:
   }
   basic_byte_buffer& swap(basic_byte_buffer& rhs) noexcept
   {
-    std::swap(_Myfirst, rhs._Myfirst);
-    std::swap(_Mylast, rhs._Mylast);
-    std::swap(_Myend, rhs._Myend);
+    char _Tmp[sizeof(rhs)];
+    memcpy(_Tmp, &rhs, sizeof(rhs));
+    memcpy(&rhs, this, sizeof(rhs));
+    memcpy(this, _Tmp, sizeof(_Tmp));
     return *this;
   }
   void insert(_Elem* where, const void* first, const void* last)
@@ -138,24 +139,24 @@ public:
     if (old_size < new_size)
       memset(_Myfirst + old_size, val, new_size - old_size);
   }
-  void resize(size_t new_size) 
+  void resize(size_t new_size)
   {
     if (this->capacity() < new_size)
-      _Reset_cap(new_size * 3 / 2);
+      _Reallocate_exactly(new_size * 3 / 2);
     _Mylast = _Myfirst + new_size;
   }
-  void resize_fit(size_t new_size) 
-  { 
+  void resize_fit(size_t new_size)
+  {
     if (this->capacity() != new_size)
-      _Reset_cap(new_size);
+      _Reallocate_exactly(new_size);
     _Mylast = _Myfirst + new_size;
   }
-  void reserve(size_t new_cap) 
+  void reserve(size_t new_cap)
   {
     if (this->capacity() < new_cap)
     {
       auto cur_size = this->size();
-      _Reset_cap(new_cap);
+      _Reallocate_exactly(new_cap);
       _Mylast = _Myfirst + cur_size;
     }
   }
@@ -178,14 +179,14 @@ public:
   }
 
 private:
-  void _Reset_cap(size_t new_cap)
+  void _Reallocate_exactly(size_t new_cap)
   {
     auto new_block = (_Elem*)realloc(_Myfirst, new_cap);
     if (new_block || 0 == new_cap)
       _Myfirst = new_block;
     else
       throw std::bad_alloc{};
-    _Myend  = _Myfirst + new_cap;
+    _Myend = _Myfirst + new_cap;
   }
 
 private:

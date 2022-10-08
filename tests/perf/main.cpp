17beed978ed39a5de374a8508692600210dc30dc
一线灵|Deal <halx99@live.com>
2022-10-09 04:52:17 +0800

    Update main.cpp

--- tests/perf/main.cpp
+++ tests/perf/main.cpp
@@ -1,1 +1,16 @@
 
+#include "yasio/detail/utils.hpp"
+#include "yasio/xxsocket.hpp"
+
+using namespace yasio;
+
+int main()
+{
+  YASIO_LOG("Testing pod_vector");
+  auto start = yasio::highp_clock();
+  
+  
+  YASIO_LOG("cost: %lf(s)\n",
+           (yasio::highp_clock() - start) / (double)std::micro::den);
+  return 0;
+}

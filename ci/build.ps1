5a0d0f8846b4026e345713ca8dfd6271c4b9537e
一线灵|Deal <halx99@live.com>
2022-10-09 05:29:03 +0800

    Update build.ps1

--- ci/build.ps1
+++ ci/build.ps1
@@ -44,11 +44,9 @@ else { # Generate vs2013 on appveyor ci
 }
 
 # Build
-cmake --build build --config $env:BUILD_TYPE --target perftest
 
-if ($env:BUILD_ARCH -eq "x64") {
-    if (!($env:UWP -eq "true")) {
-        echo 'Runing perftest ...'
-        .\build\tests\perf\Release\perftest.exe
-    }
+if (($env:BUILD_ARCH -eq "x64") -and !($env:UWP -eq "true")) {
+    echo 'Runing perftest ...'
+    cmake --build build --config $env:BUILD_TYPE --target perftest
+    .\build\tests\perf\Release\perftest.exe
 }

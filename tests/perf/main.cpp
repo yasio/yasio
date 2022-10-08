59772d7554edf036358560fc450d432abc2f56ab
一线灵|Deal <halx99@live.com>
2022-10-09 05:42:05 +0800

    Update main.cpp

--- tests/perf/main.cpp
+++ tests/perf/main.cpp
@@ -1,16 +1,44 @@
 
 #include "yasio/detail/utils.hpp"
 #include "yasio/xxsocket.hpp"
+#include "yasio/pod_vector.hpp"
+#include "yasio/xx_podvector.hpp"
 
 using namespace yasio;
 
 int main()
 {
-  YASIO_LOG("Testing pod_vector");
-  auto start = yasio::highp_clock();
-  
-  
-  YASIO_LOG("cost: %lf(s)\n",
+  int64_t count, start;
+
+  YASIO_LOG("Testing xx_pod_vector");
+  count = 0;
+  start = yasio::highp_clock();
+  for(int i = 0; i < 100000; ++i) {
+    PodVector<int> pv;
+    for (int j = 0; j < 1000; ++j) {
+      pv.Emplace(j);
+    }
+    for (int j = 0; j < 1000; ++j) {
+      count += pv[j];
+    }
+  }
+  YASIO_LOG("xxpodv --> count: %lld, cost: %lf(s)\n", count
            (yasio::highp_clock() - start) / (double)std::micro::den);
+  
+  YASIO_LOG("Testing pod_vector");
+  count = 0;
+  start = yasio::highp_clock();
+  for(int i = 0; i < 100000; ++i) {
+    ax::pod_vector<int> pv;
+    for (int j = 0; j < 1000; ++j) {
+      pv.emplace(j);
+    }
+    for (int j = 0; j < 1000; ++j) {
+      count += pv[j];
+    }
+  }
+  YASIO_LOG("axpodv --> count: %lld, cost: %lf(s)\n", count
+           (yasio::highp_clock() - start) / (double)std::micro::den);
+   
   return 0;
 }

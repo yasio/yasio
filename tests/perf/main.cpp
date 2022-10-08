7613562d0725bfe3ff93974fe607e582174d6edd
一线灵|Deal <halx99@live.com>
2022-10-09 06:30:11 +0800

    Update main.cpp

--- tests/perf/main.cpp
+++ tests/perf/main.cpp
@@ -4,13 +4,15 @@
 #include "yasio/pod_vector.hpp"
 #include "yasio/xx_podvector.hpp"
 
+#include <stdio.h>
+
 using namespace yasio;
 
 int main()
 {
   int64_t count, start;
 
-  YASIO_LOG("Testing xx_pod_vector");
+  printf("Testing xx_pod_vector\n");
   count = 0;
   start = yasio::highp_clock();
   for(int i = 0; i < 100000; ++i) {
@@ -22,10 +24,10 @@ int main()
       count += pv[j];
     }
   }
-  YASIO_LOG("xxpodv --> count: %lld, cost: %lf(s)\n", count, 
+  printf("xxpodv --> count: %lld, cost: %lf(s)\n", count, 
            (yasio::highp_clock() - start) / (double)std::micro::den);
   
-  YASIO_LOG("Testing pod_vector");
+  printf("\nTesting pod_vector\n");
   count = 0;
   start = yasio::highp_clock();
   for(int i = 0; i < 100000; ++i) {
@@ -37,7 +39,7 @@ int main()
       count += pv[j];
     }
   }
-  YASIO_LOG("axpodv --> count: %lld, cost: %lf(s)\n", count, 
+  printf("axpodv --> count: %lld, cost: %lf(s)\n", count, 
            (yasio::highp_clock() - start) / (double)std::micro::den);
    
   return 0;

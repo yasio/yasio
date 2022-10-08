4fa109ab9ea3056d7650970de51c3d811d8f3ec7
halx99 <halx99@live.com>
2022-10-09 07:18:21 +0800

    Update

--- tests/perf/main.cpp
+++ tests/perf/main.cpp
@@ -12,55 +12,67 @@ int main()
 {
   int64_t count, start;
 
-for (int k = 0; k < 5; ++k) {
-  printf("Testing xx_pod_vector...");
-  count = 0;
-  start = yasio::highp_clock();
-  for(int i = 0; i < 100000; ++i) {
-    xx::PodVector<int> pv;
-    pv.Reserve(1000);
-    for (int j = 0; j < 1000; ++j) {
-      pv.Emplace(j);
+  for (int k = 0; k < 3; ++k)
+  {
+    printf("######## Test round %d ######### \n", k + 1);
+    printf("Testing xx_pod_vector...");
+    count = 0;
+    start = yasio::highp_clock();
+    for (int i = 0; i < 100000; ++i)
+    {
+      xx::PodVector<int> pv;
+      pv.Reserve(1000);
+      for (int j = 0; j < 1000; ++j)
+      {
+        pv.Emplace(j);
+      }
+      for (int j = 0; j < 1000; ++j)
+      {
+        count += pv[j];
+      }
     }
-    for (int j = 0; j < 1000; ++j) {
-      count += pv[j];
-    }
-  }
-  printf("--> count: %lld, cost: %lf(s)\n", count, 
+    printf("--> count: %lld, cost: %lf(s)\n", count,
            (yasio::highp_clock() - start) / (double)std::micro::den);
-  
-  printf("\nTesting ax_pod_vector...");
-  count = 0;
-  start = yasio::highp_clock();
-  for(int i = 0; i < 100000; ++i) {
-    ax::pod_vector<int> pv;
-    pv.reserve(1000);
-    for (int j = 0; j < 1000; ++j) {
-      pv.emplace(j);
-    }
-    for (int j = 0; j < 1000; ++j) {
-      count += pv[j];
-    }
-  }
-  printf("--> count: %lld, cost: %lf(s)\n", count, 
-           (yasio::highp_clock() - start) / (double)std::micro::den);
-   
 
-  printf("\nTesting std_vector...");
-  count = 0;
-  start = yasio::highp_clock();
-  for(int i = 0; i < 100000; ++i) {
-    std::vector<int> pv;
-    pv.reserve(1000);
-    for (int j = 0; j < 1000; ++j) {
-      pv.emplace_back(j);
+    printf("\nTesting ax_pod_vector...");
+    count = 0;
+    start = yasio::highp_clock();
+    for (int i = 0; i < 100000; ++i)
+    {
+      ax::pod_vector<int> pv;
+      pv.reserve(1000);
+      for (int j = 0; j < 1000; ++j)
+      {
+        pv.emplace(j);
+      }
+      for (int j = 0; j < 1000; ++j)
+      {
+        count += pv[j];
+      }
     }
-    for (int j = 0; j < 1000; ++j) {
-      count += pv[j];
-    }
-  }
-  printf("--> count: %lld, cost: %lf(s)\n", count, 
+    printf("--> count: %lld, cost: %lf(s)\n", count,
            (yasio::highp_clock() - start) / (double)std::micro::den);
-}
+
+    printf("\nTesting std_vector...");
+    count = 0;
+    start = yasio::highp_clock();
+    for (int i = 0; i < 100000; ++i)
+    {
+      std::vector<int> pv;
+      pv.reserve(1000);
+      for (int j = 0; j < 1000; ++j)
+      {
+        pv.emplace_back(j);
+      }
+      for (int j = 0; j < 1000; ++j)
+      {
+        count += pv[j];
+      }
+    }
+    printf("--> count: %lld, cost: %lf(s)\n", count,
+           (yasio::highp_clock() - start) / (double)std::micro::den);
+
+    printf("\n\n");
+  }
   return 0;
 }

564ec5e0eadc32bffc9164ce0b15c83901948f38
一线灵|Deal <halx99@live.com>
2022-10-09 17:15:30 +0800

    Update pod_vector.h

--- tests/perf/pod_vector.h
+++ tests/perf/pod_vector.h
@@ -1,18 +1,18 @@
 /****************************************************************************
+ Copyright (c) Microsoft Corporation.
+ 
+ SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
  Copyright (c) 2022 Bytedance Inc.
-
  https://axmolengine.github.io/
-
+ 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
-
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
-
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,14 +20,11 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
-
  The pod_vector concepts:
    a. only accept pod type
    b. support release memory ownership with `release_pointer`
    c. support pod_allocater with C(realloc) or C++(new/delete)
-
-Copy of https://github.com/microsoft/STL/blob/main/stl/inc/vector, but only support emplace_back
-And remove many type traits processing
+Copy of https://github.com/microsoft/STL/blob/main/stl/inc/vector but for pod types and only provide 'emplace_back'
 */
 #pragma once
 #include <string.h>

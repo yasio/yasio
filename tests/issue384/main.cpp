df7cf23b57db86d6a4f7d96bf17808793f78e02d
halx99 <halx99@live.com>
2022-12-30 19:12:53 +0800

    Add socket file descriptor overflow test case

---
+++ tests/issue384/main.cpp
@@ -0,0 +1,33 @@
+#include "yasio/yasio.hpp"
+#include <iostream>
+
+using namespace yasio;
+using namespace yasio::inet;
+
+int main()
+{
+
+  // reserve 1024 socket.fds for test io service when fd greater than 1024
+  std::vector<xxsocket> socks;
+  socks.resize(1024);
+  for (int i = 0; i < 1024; ++i)
+    socks[i].open();
+
+  deadline_timer timer     = {};
+  yasio::io_service service = {};
+  service.set_option(YOPT_S_NO_NEW_THREAD, 1);
+  service.set_option(YOPT_S_DEFERRED_EVENT, 0);
+  timer.expires_from_now(std::chrono::seconds(3));
+  timer.async_wait_once(service, [](io_service& svc) { 
+	  svc.stop();
+  });
+  printf("%s", "The service will stop after 3 seconds ...\n");
+  service.start([](event_ptr&& ev) {});
+  printf("%s", "The service stopped.");
+
+  // cleanup reserved socket.fds
+  for (auto& sock : socks)
+    sock.close();
+  socks.clear();
+  return 0;
+}

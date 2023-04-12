d7e05aab969df6634329ccb5a81b5f5d4c52e3ac
Deal(一线灵) <halx99@live.com>
2023-04-12 19:35:26 +0800

    Fix ci

--- yasio/compiler/feature_test.hpp
+++ yasio/compiler/feature_test.hpp
@@ -92,7 +92,7 @@ SOFTWARE.
 #  define YASIO__HAS_UDS 0
 #endif
 
-#if (defined(__ANDROID_API__) && __ANDROID_API__ >= 21) || defined(__linux__)
+#if defined(__linux__) && !defined(__ANDROID__) || (defined(__ANDROID_API__) && __ANDROID_API__ >= 21)
 #  define YASIO__HAS_PPOLL 1
 #else
 #  define YASIO__HAS_PPOLL 0

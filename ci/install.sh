d62ac12ad175263ca495c3a98063d1afcff7b6fc
Deal(一线灵) <halx99@live.com>
2023-04-17 10:45:30 +0800

    Improve ci script

--- ci/install.sh
+++ ci/install.sh
@@ -11,8 +11,23 @@ if [[ "${RUNNER_OS}" == "Linux" && "${BUILD_TARGET}" == "android" ]]; then
 
     # install ndk
     if [ "$USE_GH_NDK" = "true" ]; then
-       echo "ndk_root=$ANDROID_NDK" >> $GITHUB_ENV
-       echo "Using gh action installed ndk"
+       echo "Find installed ndk on gh action vm ..."
+       echo "ANDROID_NDK=$ANDROID_NDK"
+       echo "ANDROID_NDK_HOME=$ANDROID_NDK_HOME"
+       echo "ANDROID_NDK_ROOT=$ANDROID_NDK_ROOT"
+
+       ndk_root=$ANDROID_NDK
+       if [ "$ndk_root" != "" ]; then
+           ndk_root=$ANDROID_NDK_HOME
+           if [ "$ndk_root" != "" ]; then
+               ndk_root=$ANDROID_NDK_ROOT
+           fi
+       fi
+    fi
+
+    if [ "$ndk_root" != "" ]; then
+        echo "Using gh action vm installed ndk: $ndk_root"
+        echo "ndk_root=$ndk_root" >> $GITHUB_ENV
     else
         curl -o ~/android-ndk-$NDK_VER-linux-x86_64.zip https://dl.google.com/android/repository/android-ndk-$NDK_VER-linux-x86_64.zip
         unzip -q ~/android-ndk-$NDK_VER-linux-x86_64.zip -d ~

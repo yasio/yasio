1cb8af7e335b2d98f24574315efcc2d8c7825161
Deal(一线灵) <halx99@live.com>
2023-06-20 13:29:44 +0800

    Update docs.ps1

--- ci/docs.ps1
+++ ci/docs.ps1
@@ -1,3 +1,6 @@
+# gen docs, requires python3.x and install dependencies:
+#  pip install -r docs/requirements.tx
+# usage: ./scripts/docs.ps1 '3.39.11:v3.39.11,3.39.12:3.39.x'
 $rel_str = $args[0]
 
 mkdocs --version
@@ -6,18 +9,19 @@ mike delete --all
 
 Write-Host "Active versions: $rel_str"
 
-$rel_arr = ($rel_str -split ',')
-if ($rel_arr.Count -eq 0) {
-    Write-Error "docs version list can't be empty, should be [docs_ver[:docs_tag],...]"
+if ("$rel_str" -eq "") {
+    Write-Error "docs versions list can't be empty, should be 'docs_ver[:docs_tag],...'"
     exit 1
 }
+
+$rel_arr = ($rel_str -split ',')
 
 $docs_ver = $null
 foreach($rel in $rel_arr) {
     # ver:tag
     $info = ($rel -split ':')
     $docs_ver = $info[0]
-    $docs_tag = if ($info.Count -ge 2) { $info[1] } else {$info[0]}
+    $docs_tag = if ($info.Count -ge 2) { $info[1] } else { "v$($info[0])" }
     git checkout "$docs_tag"
     mike deploy $docs_ver
 }
@@ -25,4 +29,4 @@ foreach($rel in $rel_arr) {
 git checkout dev
 mike deploy latest
 mike list
-# mike set-default $docs_ver --push
+mike set-default $docs_ver --push

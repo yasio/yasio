0a2d4120bf5c5355abe7c7885d1e06e56697f463
halx99 <halx99@live.com>
2023-06-20 10:58:56 +0800

    Update ci/docs.ps1

--- ci/docs.ps1
+++ ci/docs.ps1
@@ -7,13 +7,22 @@ mike delete --all
 Write-Host "Active versions: $rel_str"
 
 $rel_arr = ($rel_str -split ',')
+if ($rel_arr.Count -eq 0) {
+    Write-Error "docs versions list can't be empty, should be 'docs_ver[:docs_tag],...'
+    exit 1
+}
 
+$docs_ver = $null
 foreach($rel in $rel_arr) {
-    git checkout "v$rel"
-    mike deploy $rel
+    # ver:tag
+    $info = ($rel -split ':')
+    $docs_ver = $info[0]
+    $docs_tag = if ($info.Count -ge 2) { $info[1] } else {$info[0]}
+    git checkout "$docs_tag"
+    mike deploy $docs_ver
 }
 
 git checkout dev
 mike deploy latest
 mike list
-mike set-default $rel_arr[$rel_arr.Count - 1] --push
+# mike set-default $docs_ver --push

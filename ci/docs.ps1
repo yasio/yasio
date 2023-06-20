7f37a80393a91846bf42215cbc6bf06508b75fc5
halx99 <halx99@live.com>
2023-06-20 10:55:01 +0800

    Update mkdocs-material to v9.x

---
+++ ci/docs.ps1
@@ -0,0 +1,28 @@
+$rel_str = $args[0]
+
+mkdocs --version
+mike --version
+mike delete --all
+
+Write-Host "Active versions: $rel_str"
+
+$rel_arr = ($rel_str -split ',')
+if ($rel_arr.Count -eq 0) {
+    Write-Error "docs version list can't be empty, should be [docs_ver[:docs_tag],...]"
+    exit 1
+}
+
+$docs_ver = $null
+foreach($rel in $rel_arr) {
+    # ver:tag
+    $info = ($rel -split ':')
+    $docs_ver = $info[0]
+    $docs_tag = if ($info.Count -ge 2) { $info[1] } else {$info[0]}
+    git checkout "$docs_tag"
+    mike deploy $docs_ver
+}
+
+git checkout dev
+mike deploy latest
+mike list
+# mike set-default $docs_ver --push

42f3e2967118e39a22e0b1d1a573db851cc0e57c
halx99 <halx99@live.com>
2023-10-05 15:31:59 +0800

    Add comment

--- yasio/string.hpp
+++ yasio/string.hpp
@@ -1,3 +1,43 @@
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// A multi-platform support c++11 library with focus on asynchronous socket I/O for any
+// client application.
+//////////////////////////////////////////////////////////////////////////////////////////
+/*
+The MIT License (MIT)
+
+Copyright (c) 2012-2023 HALX99
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+The yasio dedicated string (API not 100% compatible with stl) concepts:
+   a. no SSO, no COW, sizeof(yasio::string) = 16(x64), 12(x86)
+   b. The resize behavior differrent stl, always allocate exactly
+   c. By default resize without fill (uninitialized and for overwrite)
+   d. Support release internal buffer ownership with `release_pointer`
+   e. Transparent iterator
+   f. The operations expand/append/insert/push_back/+= will trigger memory allocate growth strategy MSVC
+   g. resize_and_overwrite (c++23)
+   h. provide API replace_all, to_upper, to_lower which stl not provide
+   i. More suitable for small text file (> SSO size) read API
+*/
+
 #pragma once
 
 #include <ctype.h>

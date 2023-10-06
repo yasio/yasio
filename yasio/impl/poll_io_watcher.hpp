b6c7e40aca9297fef8d5becda0de844dffdf88df
halx99 <halx99@live.com>
2023-10-07 00:25:53 +0800

    Improve code style

--- yasio/impl/poll_io_watcher.hpp
+++ yasio/impl/poll_io_watcher.hpp
@@ -91,7 +91,7 @@ protected:
     {
       auto events = add_events & ~remove_events;
       if (events)
-        fdset.push_back(pollfd{fd, static_cast<short>(events), static_cast<short>(0)});
+        fdset.emplace_back(fd, static_cast<short>(events), static_cast<short>(0));
     }
   }
 

030ff33fe858ae32ffb1af622e72bb5af1406f04
Deal <halx99@live.com>
2024-03-11 22:54:31 +0800

    Don't append '\n' for emscripten
    because the browser console is android logcat like

--- yasio/logging.hpp
+++ yasio/logging.hpp
@@ -30,7 +30,7 @@ SOFTWARE.
 #include "yasio/strfmt.hpp"
 
 #if defined(__EMSCRIPTEN__)
-#  define YASIO_LOG_TAG(tag, format, ...) printf((tag format "\n"), ##__VA_ARGS__)
+#  define YASIO_LOG_TAG(tag, format, ...) printf((tag format), ##__VA_ARGS__)
 #elif defined(_WIN32)
 #  define YASIO_LOG_TAG(tag, format, ...) OutputDebugStringA(::yasio::strfmt(127, (tag format "\n"), ##__VA_ARGS__).c_str())
 #elif defined(ANDROID) || defined(__ANDROID__)

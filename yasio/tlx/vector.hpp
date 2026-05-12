eb4c6d59c550807e8299130cf0edc259f9f4d4f7
halx99 <halx99@live.com>
2026-05-12 11:51:47 +0800

    fixup

--- yasio/tlx/vector.hpp
+++ yasio/tlx/vector.hpp
@@ -570,7 +570,7 @@ public:
     return *this;
   }
 
-  template <typename = enable_if_t<std::is_pointer_v<_Ty>>>
+  template <typename _U = value_type, std::enable_if_t<std::is_pointer_v<_U>, int> = 0>
   constexpr void resize(const size_type _Newsize, std::nullptr_t)
   {
     _Resize(_Newsize, _TLX value_init);
